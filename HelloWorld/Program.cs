f00a5b9abf17a431ae175dd501f36dfc6ed54d1d
Daniel <danielgonzalez.l@hotmail.com>
2025-03-13 14:21:10 -0500

    Chance on Helloword by Calss

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace Helloworld{
+    class Program{
+        static void Main(){
+            var name = "Juana";
+            Console.WriteLine("Hello Birdth" + ( name));
+            Console.WriteLine();
+        }
+    }
+}
