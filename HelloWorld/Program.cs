81567cd21f051627caa935ea91deb09764903b73
Daniel <danielgonzalez.l@hotmail.com>
2025-03-17 14:04:55 -0500

    Add new code about Age Calculator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,41 @@
 ﻿namespace Helloworld{
     class Program{
-        static void Main(){
-            var name = "Juana";
-            Console.WriteLine("Hello Birdth" + ( name));
+         static void Main(){
+            DateOnly dateConverted = new DateOnly() ;
+            string nameInput;
+            String birthdayDate;
+            bool isDateValid = false;
+
+            Console.WriteLine(" welcome to calculator of Age");
+            Console.WriteLine(" Writte your name.");
+            nameInput = Console.ReadLine();
+            Console.WriteLine("It is a pleasure will have haere you " +( nameInput) );  
+            
+            while (!isDateValid){
+                Console.WriteLine("Tell me your Birthday date please in format dd/mm/yy");
+                birthdayDate = Console.ReadLine();
+                isDateValid = DateOnly.TryParse(birthdayDate,out dateConverted);
+            if(!isDateValid) Console.WriteLine("this date is incorrect, the correct format is:" + (dateConverted));
+            else Console.WriteLine($"The date {dateConverted} is valid.");
+            }
             Console.WriteLine();
-        }
+
+            var person = new Person{
+                name = nameInput,
+                birthday = dateConverted,
+                age = DateTime.Now.Year-dateConverted.Year, 
+            };
+            Console.WriteLine($"Your name is {person.name}");
+            Console.WriteLine($"Your birthday is {person.birthday}");
+            Console.WriteLine($"Your Age is {person.age}");
+
+            
+                 }
+    }
+
+    class Person{
+        public string name { get; set; }
+        public int age { get; set; }
+        public DateOnly birthday { get; set; }
     }
 }
